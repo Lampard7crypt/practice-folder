11737c905e0c042f5361c0bff090e36c2f5d1298
Lampard <lampardkipyegon@gmail.com>
2025-11-23 08:04:53 +0300

    Audio TRanscription with assemblyai

--- all_files/prac5.py
+++ all_files/prac5.py
@@ -1,13 +1,19 @@
-import matplotlib.pyplot as plt 
- 
-girls_grades = [89, 90, 70, 89, 100, 80, 90, 100, 80, 34] 
-boys_grades = [30, 29, 49, 48, 100, 48, 38, 45, 20, 30] 
-grades_range = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100] 
-fig=plt.figure() 
-ax=fig.add_axes([0,0,1,1]) 
-ax.scatter(grades_range, girls_grades, color='r') 
-ax.scatter(grades_range, boys_grades, color='b') 
-ax.set_xlabel('Grades Range') 
-ax.set_ylabel('Grades Scored') 
-ax.set_title('scatter plot') 
-plt.show()
+# Install the assemblyai package by executing the command "pip install assemblyai"
+
+# Come to this later
+
+import assemblyai as aai
+
+aai.settings.api_key = "12d38a465e2648889e760ef08a15ca12"
+
+# audio_file = "./local_file.mp3"
+audio_file = "https://assembly.ai/wildfires.mp3"
+
+config = aai.TranscriptionConfig(speech_model=aai.SpeechModel.universal)
+
+transcript = aai.Transcriber(config=config).transcribe(audio_file)
+
+if transcript.status == "error":
+  raise RuntimeError(f"Transcription failed: {transcript.error}")
+
+print(transcript.text)
