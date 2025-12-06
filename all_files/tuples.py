1eaa61dfdc26942a6e2b97259dcac0d6fa319bc4
Lampard <lampardkipyegon@gmail.com>
2025-12-06 11:55:01 +0300

    Created new repo for C file, removed them here, edited tuples.py file

--- all_files/tuples.py
+++ all_files/tuples.py
@@ -3,4 +3,12 @@
 def sort_by_age(s: list):
     return sorted(s, key=lambda item: item[1])
 
-print(sort_by_age([('Alice', 30), ('Bob', 25), ('Eve', 35)]))
+# print(sort_by_age([('Alice', 30), ('Bob', 25), ('Eve', 35)]))
+
+def count_vowels(s: str) -> str:
+    return len([letter for letter in s if letter in ['a', 'e', 'i', 'o', 'u']])
+
+# print(count_vowels("vowel"))
+
+
+
